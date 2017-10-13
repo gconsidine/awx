44d8790f6546004c07deb42fa90daaa32b53627a
gconsidine <greg@gregconsidine.com>
2017-10-13 14:30:20 -0400

    Add support for local and brand config overrides

--- awx/ui/client/start/config.js
+++ awx/ui/client/start/config.js
@@ -1,11 +1,29 @@
 import _ from 'lodash';
 
-const ENV = process.env.NODE_ENV || 'development';
-
 import base from '~config/config.base';
+
+const ENV = process.env.NODE_ENV || 'development';
 const env = require(`~config/config.${ENV}`);
 
-const config = _.merge(base, env);
+let config = _.merge(base, env);
+let local;
+let brand;
+
+try {
+    local = require('~config/config.local');
+} catch (err) { }
+
+try {
+    brand = require('~config/config.brand.json');
+} catch (err) { }
+
+if (brand) {
+    config = _.merge(config, brand);
+}
+
+if (local) {
+    config = _.merge(config, local);
+}
 
 function get (key) {
     if (typeof key === 'undefined') {
